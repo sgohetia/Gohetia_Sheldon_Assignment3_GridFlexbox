59f95f338172df6dba39d98caa9789fde2aa9eb5
sheldongohetia <sheldongohetia.ca@gmail.com>
2024-07-24 13:46:04 -0400

    Update main.js
    updated JS

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@
 
   //This function here is for the burger menu icon toggled when the user clicks
   // main.js
-  const burgerMenu = document.querySelector("burger-menu");
-  const mobileNavLinks = document.querySelector("mobile-nav-links");
+  const burgerMenu = document.querySelector("#burger-menu");
+  const mobileNavLinks = document.querySelector("#mobile-nav-links");
 
   burgerMenu.addEventListener("click", function () {
     mobileNavLinks.classList.toggle("hidden");
